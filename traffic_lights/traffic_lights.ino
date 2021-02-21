22a48571ebdebc6ce2e6ea4bfe63a446d92fb4d1
JoshuaHartling <Joshua_Hartling@student.uml.edu>
2021-02-21 17:24:09 -0500

    lab 1 - traffic light - 2021-21-02 17:24

--- traffic_lights/traffic_lights.ino
+++ traffic_lights/traffic_lights.ino
@@ -1,55 +1,53 @@
-int redLED=2; // red LED assigned to pin 2
-int yellowLED=3; // yellow LED assigned to pin 3
-int greenLED=4; // green LED assigned to pin 4
-int button=5; // button assigned to pin 5
-int buzzer=12; // buzzer assigned to pin 12
+int redLED = 2; // red LED assigned to pin 2
+int yellowLED = 3; // yellow LED assigned to pin 3
+int greenLED = 4; // green LED assigned to pin 4
+int button = 5; // button assigned to pin 5
+int buzzer = 12; // buzzer assigned to pin 12
 
 void setup() {
+  bool isWait = true;
   pinMode(redLED, OUTPUT);
   pinMode(yellowLED, OUTPUT);
   pinMode(greenLED, OUTPUT);
   pinMode(buzzer, OUTPUT);
   pinMode(button, INPUT_PULLUP);
+
+  // if button is pressed, begin traffic lights simulation
+  while (isWait)
+  {
+    if (digitalRead(button) == LOW)
+    {
+     isWait = false; 
+    }
+  }
 }
 
 void loop() {
-  
-  // flashing red LED
-  digitalWrite(redLED, HIGH);   // turn the red LED on 
-  delay(1000);                       // wait for a second
-  digitalWrite(redLED, LOW);    // turn red the LED off 
-  delay(1000);                       // wait for a second
-
-  // if button is pressed, begin traffic lights simulation
-  if (digitalRead(button) == LOW)
-  {
     digitalWrite(redLED, HIGH); // turn red LED on
-    delay(15000);               // leave on for 15 seconds
+    delay(12000);               // leave on for 15 seconds
+    buzzerAlert();                 // alert that lights are changing
     digitalWrite(redLED, LOW); // turn red LED off
-
-    // call buzzerAlert() function to alert that lights are changing
-    buzzerAlert();
     
     digitalWrite(greenLED, HIGH); // turn green LED on
-    delay(15000);                 // leave on for 15 seconds
+    delay(12000);                 // leave on for 15 seconds
+    buzzerAlert();                 // alert that lights are changing
     digitalWrite(greenLED, LOW);  // turn green LED off
-
-    // call buzzerAlert() function to alert that lights are changing
-    buzzerAlert();
-
+  
     // call buzzerAlert() function to alert that lights are changing
     digitalWrite(yellowLED, HIGH); // turn yellow LED on
-    delay(15000);                  // leave on for 15 seconds
+    delay(1000);                  // leave on for 15 seconds
+    buzzerAlert();                 // alert that lights are changing
     digitalWrite(yellowLED, LOW);  // turn yellow LED off
-
-    buzzerAlert();
-  }
 }
 
 // function for turning buzzer on for 3 seconds before a light changes
 void buzzerAlert() {
-  
-      digitalWrite(buzzer,HIGH); // turn buzzer on
-      delay(3000); // wait for 3 seconds
-      digitalWrite(buzzer,LOW); // turn buzzer off
+    int i;
+    for (i=0; i < 1500; i++)
+    {
+      digitalWrite(buzzer,HIGH);
+      delay(1);//wait for Xms
+      digitalWrite(buzzer,LOW);
+      delay(1);//wait for Xms
+    }
 }
